f739b71d0da3dc1c33033c7f0f1ea07e474fb586
X400X <132714744+X400X@users.noreply.github.com>
2024-02-06 11:47:46 +0200

    Update 05.02.2024 main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+import calendar
+from datetime import datetime
+import os
 import re
 import time
 
 date = []
+date_del = []
 selected_event = []
 
 
 def load_dates():
-    with open("date.txt") as file:
-        for row in file:
-            sdata = row.strip().split("|")
-            d = (sdata[0], sdata[1], sdata[2], sdata[3], int(sdata[4]))
-            date.append(d)
+    if os.path.exists("date.txt"):
+        with open("date.txt") as file:
+            for row in file:
+                sdata = row.strip().split("|")
+                d = (sdata[0], sdata[1], sdata[2], sdata[3], int(sdata[4]))
+                date.append(d)
 
 
 def save_dates():
@@ -97,78 +102,257 @@ def check_imp(i: int):
     return a
 
 
-def search_date(s: str):
+def search_all(x: str, xt: int):
+    if xt == 4:
+        sss = int(x)
+    else:
+        sss = x
     a = True
     for i in range(len(date)):
-        if date[i][0] == s:
+        if date[i][xt] == sss:
             print(date[i][0] + " " + date[i][1] + " " + date[i][2] + " " + date[i][3] + " " + str(date[i][4]))
             a = False
+            d = (date[i][0], date[i][1], date[i][2], date[i][3], int(date[i][4]))
+            date_del.append(d)
     if a:
         print("Not found.")
 
 
-def search_eve(s: str):
-    a = True
-    for i in range(len(date)):
-        if date[i][2] == s:
-            print(date[i][0] + " " + date[i][1] + " " + date[i][2] + " " + date[i][3] + " " + str(date[i][4]))
-            a = False
-    if a:
-        print("Not found.")
+def delete_date():
+    b = 0
+    while b < len(date_del):
+        a = 0
+        while a < len(date):
+            if date_del[b] == date[a]:
+                del date[a]
+            else:
+                a += 1
+        b = b + 1
+    date_del.clear()
 
 
-def search_imp(x: int):
-    a = True
-    for i in range(len(date)):
-        if date[i][4] == x:
-            print(date[i][0] + " " + date[i][1] + " " + date[i][2] + " " + date[i][3] + " " + str(date[i][4]))
-            a = False
-    if a:
-        print("Not found.")
+def go_to_start():
+    print("Go to start.")
+
+
+def show_events_for_delete():
+    b = 0
+    while b < len(date_del):
+        print(date_del[b][0] + " " + date_del[b][1] + " " + date_del[b][2] + " " + date_del[b][3] + " " + str(
+            date_del[b][4]))
+        h = int(input(f"Do you want to delete this event?\n1 - Yes\n2 - Continue\nOption: "))
+        try:
+            if not 0 < h < 3:
+                print("The Choose entered is incorrect. The Choose should be 1 or 2")
+            if h == 1:
+                d = (date_del[b][0], date_del[b][1], date_del[b][2], date_del[b][3], int(date_del[b][4]))
+                date_del.clear()
+                date_del.append(d)
+                delete_date()
+                print("Selected event are deleted.")
+            if h == 2:
+                print("Next")
+        except ValueError:
+            print("The Choose entered is incorrect. The Choose should be 1 or 2")
+        b += 1
+
+
+def delete_step_by_step(y: int):
+    s0 = f"\nDo you want to delete all events for this date?\n1 - Yes\n2 - Select event\n0 - To Start\nOption: "
+    s2 = f"\nDo you want to delete all of this same names events?\n1 - Yes\n2 - Select event\n0 - To Start\nOption: "
+    s4 = f"\nDo you want to delete all events for this importance?\n1 - Yes\n2 - Select event\n0 - To Start\nOption: "
+    w = ""
+    if y == 0:
+        w = s0
+    elif y == 2:
+        w = s2
+    else:
+        w = s4
+    try:
+        h = int(input(w))
+        if not -1 < how < 3:
+            print("The Choose entered is incorrect. The Choose should be from 0 to 2")
+        if h == 1:
+            delete_date()
+            print("All selected events are deleted.")
+        if h == 2:
+            show_events_for_delete()
+        if h == 0:
+            go_to_start()
+            date_del.clear()
+    except ValueError:
+        print("The Choose entered is incorrect. The Choose should be from 0 to 2")
+
+
+def set_str1(ss: str):
+    t = ss.split(".")
+    s1 = "______________________________________\n"
+    s1 = s1 + "|      Month: " + t[1] + "     Year: " + t[2] + "      |\n"
+    s1 = s1 + "|____________________________________|\n"
+    s1 = s1 + "|  Ma   Ti   Ke   To   Pe   La   Su  |\n"
+    s1 = s1 + "| "
+    a = 1
+    b = 1
+    num_days = calendar.monthrange(int(t[2]), int(t[1]))[1]
+    d = "01." + t[1] + "." + t[2]
+    date2 = datetime.strptime(d, "%d.%m.%Y")
+    i = date2.weekday()
+    s = "    "
+    while i > 0:
+        s1 = s1 + s + " "
+        i -= 1
+        a += 1
+
+    while a < 8:
+        s1 = s1 + return_number_of_day(b, t[1], t[2]) + " "
+        b += 1
+        a += 1
+    s1 = s1 + "|\n| "
+
+    while a < 15:
+        s1 = s1 + return_number_of_day(b, t[1], t[2]) + " "
+        b += 1
+        a += 1
+    s1 = s1 + "|\n| "
+
+    while a < 22:
+        s1 = s1 + return_number_of_day(b, t[1], t[2]) + " "
+        b += 1
+        a += 1
+    s1 = s1 + "|\n| "
+
+    while a < 29:
+        s1 = s1 + return_number_of_day(b, t[1], t[2]) + " "
+        b += 1
+        a += 1
+    s1 = s1 + "|\n"
+
+    if num_days != b:
+        s1 = s1 + "| "
+        while a < 36:
+            if b <= num_days:
+                s1 = s1 + return_number_of_day(b, t[1], t[2]) + " "
+            else:
+                s1 = s1 + s + " "
+            b += 1
+            a += 1
+        s1 = s1 + "|\n"
+
+    if b <= num_days:
+        s1 = s1 + "| "
+        while a < 43:
+            if b <= num_days:
+                s1 = s1 + return_number_of_day(b, t[1], t[2]) + " "
+            else:
+                s1 = s1 + s + " "
+            b += 1
+            a += 1
+        s1 = s1 + "|\n"
+
+    s1 = s1 + "|____________________________________|"
+
+    return s1
+
+
+def one_equal(s: str):
+    a = False
+    for iii in range(len(date)):
+        if s == date[iii][0]:
+            a = True
+    return a
+
+
+def return_number_of_day(i: int, a: str, b: str):
+    s3 = ""
+    s = ""
+    if i < 10:
+        s = "0" + str(i)
+    else:
+        s = str(i)
+    s3 = s + "." + a + "." + b
+    if one_equal(s3):
+        s = "|" + s + "|"
+    else:
+        s = " " + s + " "
+    return s
 
 
 load_dates()
-
-while True:
-    s = int(input(f"Choose:\n1 - Add new event\n2 - Show all events\n3 - Search\n0 - Exit\nOption: "))
-    if not isinstance(s, int):
-        print("The Choose entered is incorrect. The Choose should be from 0 to 5")
-    if not -1 < s < 5:
-        print("The Choose entered is incorrect. The Choose should be from 0 to 5")
-    if s == 0:
-        save_dates()
-        break
-    if s == 1:
-        dat = input("Date: ")
-        dat1 = get_date(dat)
-        if check_date(dat):
-            tim = input("Time: ")
-            tim1 = get_time(tim)
-            if check_time(tim):
-                event = input("Event: ")
-                descr = input("Description: ")
-                imp = int(input("Importance: "))
-                if check_imp(imp):
-                    dd = (dat1, tim1, event, descr, imp)
-                    date.append(dd)
-    if s == 2:
-        for i in range(len(date)):
-            print(date[i][0] + " " + date[i][1] + " " + date[i][2] + " " + date[i][3] + " " + str(date[i][4]))
-    if s == 3:
-        how = int(input(f"Choose:\n1 - Date\n2 - Event\n3 - Importance\nOption: "))
-        if not isinstance(how, int):
-            print("The Choose entered is incorrect. The Choose should be from 1 to 3")
-        if not 0 < how < 4:
-            print("The Choose entered is incorrect. The Choose should be from 1 to 3")
-        if how == 1:
-            date_t = input("Date: ")
-            date_t1 = get_date(date_t)
-            if check_date(date_t):
-                search_date(date_t1)
-        if how == 2:
-            eve_t = input("Event: ")
-            search_eve(eve_t)
-        if how == 3:
-            imp_t = input("Importance: ")
-            if check_imp(int(imp_t)):
-                search_imp(int(imp_t))
+try:
+    while True:
+        try:
+            s = int(input(
+                f"Choose:\n1 - Add new event\n2 - Show all events\n3 - Search and Delete\n0 - Exit\nOption: "))
+            if not -1 < s < 4:
+                print("The Choose entered is incorrect. The Choose should be from 0 to 3")
+            if s == 0:
+                save_dates()
+                break
+            if s == 1:
+                dat = input("Date: ")
+                dat1 = get_date(dat)
+                if check_date(dat):
+                    tim = input("Time: ")
+                    tim1 = get_time(tim)
+                    if check_time(tim):
+                        event = input("Event: ")
+                        descr = input("Description: ")
+                        imp = int(input("Importance: "))
+                        if check_imp(imp):
+                            dd = (dat1, tim1, event, descr, imp)
+                            date.append(dd)
+                            print("Successfully added!")
+            if s == 2:
+                try:
+                    how = int(input(f"Choose:\n1 - Current month and year\n2 - Any date\n3 - All events in list\n0 - To Start\nOption: "))
+                    if not -1 < how < 4:
+                        print("The Choose entered is incorrect. The Choose should be from 0 to 3")
+                    if how == 1:
+                        date123 = datetime.today()
+                        str1 = date123.strftime(".%m.")
+                        str2 = date123.strftime("%y")
+                        s = "01" + str1 + "20" + str2
+                        date_t4 = get_date(s)
+                        if check_date(s):
+                            print(set_str1(date_t4))
+                    if how == 2:
+                        date_t = input("Date: ")
+                        date_t3 = get_date(date_t)
+                        if check_date(date_t3):
+                            print(set_str1(date_t3))
+                    if how == 3:
+                        for i in range(len(date)):
+                            print(date[i][0] + " " + date[i][1] + " " + date[i][2] + " " + date[i][3] + " " + str(date[i][4]))
+                    if how == 0:
+                        go_to_start()
+                except ValueError:
+                    print("The Choose entered is incorrect. The Choose should be from 0 to 3")
+            if s == 3:
+                try:
+                    how = int(input(f"Choose:\n1 - Date\n2 - Event\n3 - Importance\n0 - To Start\nOption: "))
+                    if not -1 < how < 4:
+                        print("The Choose entered is incorrect. The Choose should be from 0 to 3")
+                    if how == 1:
+                        date_t = input("Date: ")
+                        date_t1 = get_date(date_t)
+                        if check_date(date_t):
+                            search_all(date_t1, 0)
+                            delete_step_by_step(0)
+                    if how == 2:
+                        eve_t = input("Event: ")
+                        search_all(eve_t, 2)
+                        delete_step_by_step(2)
+                    if how == 3:
+                        imp_t = input("Importance: ")
+                        if check_imp(int(imp_t)):
+                            search_all(imp_t, 4)
+                            delete_step_by_step(4)
+                    if how == 0:
+                        go_to_start()
+                        date_del.clear()
+                except ValueError:
+                    print("The Choose entered is incorrect. The Choose should be from 0 to 3")
+        except ValueError:
+            print("The Choose entered is incorrect. The Choose should be from 0 to 3")
+except KeyboardInterrupt:
+    print("Something wrong")
